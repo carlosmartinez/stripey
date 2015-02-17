f4a20931ae54b964ed7190d21267e611ca2137bb
Carlos Martinez <carlosmartinez@fullsixuk.com>
2015-02-17 16:52:55 +0000

    stripe mock

--- lib/stripe_payments_api.rb
+++ lib/stripe_payments_api.rb
@@ -1,3 +1,6 @@
 module StripePaymentsApi
-
+	def self.create_account(token)
+      customer = Stripe::Customer.create(description: 'dealership.name', card: token)
+      customer
+	end
 end
--- spec/features/payments_spec.rb
+++ spec/features/payments_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 require 'stripe_payments_api'
 require 'factory_girl_rails'
+require 'stripe_mock'
 
 describe StripePaymentsApi, :type => :feature do
+
+  let(:stripe_helper) { StripeMock.create_test_helper }
+  let(:token) { stripe_helper.generate_card_token }
+  before { StripeMock.start }
+  after { StripeMock.stop }
 
   let(:producer) { create(:producer) }
   let(:line_item) { create(:line_item, producer: producer) }
   let(:payment) { create(:payment, line_item: line_item) }
 
-  it 'does not very much' do
-    expect(producer.name).to eq 'Arbitrary Producer'
+  it 'creates recipient account' do
+    customer = StripePaymentsApi::create_account token
+    expect(customer.cards.total_count).to eq 1
   end
+
 end
